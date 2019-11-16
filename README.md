c25dd79346a01b594f7d30810ba2f7b099c5cf76
Paul Luca <luca95.paul@yahoo.com>
2019-11-16 10:53:48 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Thermometer Application
+
+A python aplication that presents a thermometer
+## Subtitle
+
+##Subtitle
+
+'''python 
+
+import date
+
+class Temp():
+    pass
+    
---
+++ thermometer.py
@@ -0,0 +1,18 @@
+
+
+class Thermometer:
+    def __init__(self, temperature = 0):
+        self.temperature = temperature
+
+    def get_temp():
+        pass
+
+    def set_temp():
+        pass
+
+    def to_celsius(self):
+        pass
+
+    def to_fahrenheit(self):
+        pass
+    
