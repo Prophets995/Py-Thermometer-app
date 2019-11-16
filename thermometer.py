7416eb26465bff9fb28aaabf5e8b6a0755dfeed5
Paul Luca <luca95.paul@yahoo.com>
2019-11-16 12:03:55 +0200

    Adauga logica

--- thermometer.py
+++ thermometer.py
@@ -1,18 +1,23 @@
-
-
 class Thermometer:
     def __init__(self, temperature = 0):
         self.temperature = temperature
 
-    def get_temp():
-        pass
+    def get_temperature(self, unit="C"):
+        if unit == "C":
+            return self.temperature
+        elif unit == "F":
+            return self.to_fahrenheit(self.temperature )
 
-    def set_temp():
-        pass
+    def set_temperature(self, temperature):
+        self.temperature = temperature
 
-    def to_celsius(self):
-        pass
 
-    def to_fahrenheit(self):
-        pass
-    
+    def to_celsius(self, temperature):
+        temperature = (temperature - 32) * 5 / 9
+        return temperature
+
+
+    def to_fahrenheit(self, temperature):
+        temperature = temperature * 9 / 5 + 32
+        return temperature
+
